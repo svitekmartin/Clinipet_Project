2e8d9cff73f8353f33f868cf6cc6c8beb2aafc82
Martin <m.svitek@gmail.com>
2025-05-23 20:54:34 +0200

    Update Step_1.sql

--- Step_1.sql
+++ Step_1.sql
@@ -1,21 +1,23 @@
 
  - Clean missing/wrong values in reg_cards 
 SELECT
-patient_id, 
-owner_id, 
-TRIM(REGEXP_REPLACE(owner_name, r'\b(Dr\.?|PhD|Prof\.?|MD)\b', '')) AS owner_name,
-pet_type,
--- Replace missing breed with 'Unknown' 
-IFNULL(breed,'Unknown') AS breed,
--- Make patient_names consistent
-UPPER(SUBSTR(patient_name, 1, 1)) || LOWER(SUBSTR(patient_name, 2)) AS patient_name,
-gender, 
-patient_age, 
-date_registration,
--- Remove non-numeric values from owner_phone
-REGEXP_REPLACE(owner_phone, r'[^0-9]', '') AS owner_phone
+  patient_id,
+  owner_id,
+  TRIM(REGEXP_REPLACE(owner_name, r'\b(Dr\.?|PhD|Prof\.?|MD)\b', '')) AS owner_name,
+  pet_type,
+  -- Replace missing breed with 'Unknown'
+  IFNULL(breed,'Unknown') AS breed,
+  -- Make patient_names consistent
+  UPPER(SUBSTR(patient_name, 1, 1)) || LOWER(SUBSTR(patient_name, 2)) AS patient_name,
+  gender,
+  patient_age,
+  date_registration,
+  -- Remove non-numeric values from owner_phone
+  REGEXP_REPLACE(owner_phone, r'[^0-9]', '') AS owner_phone
+FROM
+  `martins-test-laut-campus.health_tail.healthtail_reg_cards`
 
-FROM `martins-test-laut-campus.health_tail.healthtail_reg_cards`
+  
 
 CREATE OR REPLACE TABLE `martins-test-laut-campus.health_tail_consumer.med_audit` AS
 
